b6bb0fee3c9f5dbe36998d2a1c9991b33694818f
ezerevello <revellojonathan@gmail.com>
2026-03-15 04:38:05 -0300

    feat: add initial QR code generator from user input

--- cmd/terminal/main.go
+++ cmd/terminal/main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"NanoQR/internal/service"
+	"fmt"
+	"os"
+	qrcode "github.com/skip2/go-qrcode"
+)
+
+func main () {
+	// Asks for the URL
+	input, err := service.ReadString(os.Stdin, "Ingresa el enlace: ")
+
+	// Error
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return
+	}
+	
+	// Succes
+	png, err := qrcode.Encode(input, qrcode.Medium, 256)
+
+		// Error generating QR code
+		if err != nil {
+			fmt.Printf("Error: %v", err)
+		}
+
+		// Succes
+		fmt.Print("qr:\n", png)
+}
